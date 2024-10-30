b78abf0ed21aa7563bb4b69a33ab43dd49d43fed
Lucas Bernadino <luucasdinoo@gmail.com>
2024-10-29 21:37:42 -0300

    useCase de importação de um arquivo csv

--- src/modules/car/useCases/importCategory/index.ts
+++ src/modules/car/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
 import { ImportCategoryService } from "./ImportCategoryService";
 import { ImportCategoryController } from "./ImportCategoryController";
+import { CategoryRepository } from "../../repositories/CategoryRepository";
 
-const importCategoryService = new ImportCategoryService();
+const categoryRepository = CategoryRepository.getInstance();
+const importCategoryService = new ImportCategoryService(categoryRepository);
 const importCategoryController = new ImportCategoryController(importCategoryService);
 
 export { importCategoryController };
